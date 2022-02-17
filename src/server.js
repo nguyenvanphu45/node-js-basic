a906a7738074605ba7570e7e31fc90d060704e42
Phu <vanphu040520022gmail.com>
2022-02-17 15:15:54 +0700

    routing and MVC

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 import express from 'express'
 import configViewEngine from './config/viewEngine'
+import initWebRoute from './route/web'
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080;
 
+// setup view engine
 configViewEngine(app)
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-
-app.get('/about', (req, res) => {
-    res.send(`I'm Phu!`)
-})
+// init web route
+initWebRoute(app)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://locahost:${port}`)
