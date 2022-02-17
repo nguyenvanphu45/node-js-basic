29fb8913d7c5a268a867f2e134f98c219592221d
Phu <vanphu040520022gmail.com>
2022-02-17 11:14:07 +0700

    view engine

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-const express = require('express')
+import express from 'express'
+import configViewEngine from './config/viewEngine'
 const app = express()
 const port = 8080
 
+configViewEngine(app)
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('index.ejs')
 })
 
 app.get('/about', (req, res) => {
