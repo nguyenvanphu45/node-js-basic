d86523a9500a36d4a9f68bafa81e7367f69b078f
Phu <vanphu040520022gmail.com>
2022-02-20 14:36:54 +0700

    detail users && routers params

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,29 +1,17 @@
-import connection from '../config/connectDB';
+import pool from '../config/connectDB';
 
-let getHomepage = (req, res) => {
-    //logic
-    let data = [];
-    connection.query(
-        'SELECT * FROM `users` ',
-        function (err, results, fields) {
-            results.map((row) => {
-                data.push({
-                    id: row.id,
-                    email: row.email,
-                    address: row.address,
-                    firstName: row.firstName,
-                    lastName: row.lastName
-                })
+let getHomepage = async (req, res) => {
+    const [rows, fields] = await pool.execute('SELECT * FROM users')
+    return res.render('index.ejs', { dataUser: rows })
+}
 
-            });
+let getDetailPage = async (req, res) => {
+    let userId = req.params.id
+    let [user] = await pool.execute(`select * from users where id = ?`, [userId])
 
-            return res.render('index.ejs', { dataUser: data })
-
-        })
-
-
+    return res.send(JSON.stringify(user))
 }
 
 module.exports = {
-    getHomepage
+    getHomepage, getDetailPage
 }
