4f2b19308d99aab36f0dc59c4ed94501f44f8eda
Phu <vanphu040520022gmail.com>
2022-02-20 21:15:36 +0700

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -22,6 +22,30 @@ let createNewUser = async (req, res) => {
     return res.redirect('/')
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId
+
+    await pool.execute('Delete from users where id = ? ', [userId])
+
+    return res.redirect('/')
+}  
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id
+    let [user] = await pool.execute('Select * from users where id = ?', [id])
+
+    return res.render('update.ejs', { dataUser: user[0] })
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body
+
+    await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?',
+        [firstName, lastName, email, address, id])
+
+    return res.redirect('/')
+}
+
 module.exports = {
-    getHomepage, getDetailPage, createNewUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
 }
