2b33e5676577ec41ad526f94c6f37fd95ec1ea3f
Phu <vanphu040520022gmail.com>
2022-02-22 22:18:57 +0700

    upload multiple files

--- src/route/web.js
+++ src/route/web.js
@@ -25,6 +25,7 @@ const imageFilter = function(req, file, cb) {
 }
 
 let upload = multer({ storage: storage, fileFilter: imageFilter })
+let upload1 = multer({ storage: storage, fileFilter: imageFilter }).array('multiple_image', 3)
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage)
@@ -37,6 +38,21 @@ const initWebRoute = (app) => {
 
     router.get('/upload', homeController.getUploadFilePage)
     router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+
+    router.post('/upload-multiple-images', (req, res, next) => {
+        upload1(req, res, (err) => {
+            if (err instanceof multer.MulterError && err.code === "LIMIT_UNEXPECTED_FILE") {
+                // handle multer file limit error here
+                res.send("LIMIT_UNEXPECTED_FILE")
+            } else if (err) {
+                res.send(err)
+            }
+            else {
+                // make sure to call next() if all was well
+                next();
+            }
+        })
+    }, homeController.handleUploadMultipleFiles)
 
     router.get('/about', (req, res) => {
         res.send(`I'm Phu!`)
